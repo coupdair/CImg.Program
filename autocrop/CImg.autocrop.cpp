926625a2d6b198e44e6d54b4fdec7761a354af34
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2023-10-10 11:40:41 +0200

    autocrop: full auto (auto color)

--- autocrop/CImg.autocrop.cpp
+++ autocrop/CImg.autocrop.cpp
@@ -60,54 +60,21 @@ using namespace cimg_library;
 
 // Main procedure
 int main(int argc,char **argv) {
-  cimg_usage("crop\n"
+  cimg_usage("autocrop\n"
   "  examples:\n"
-  "  ./CImg.crop -i logo -e .png -x 111 -y 55 -o _ #crop 4 quadrant\n"
-  "  ./CImg.crop -i logo.png     -x 111 -y 55 -sx 128 -sy 64 -o logo_crop.png #crop rectangle\n"
+  "  ./CImg.crop -i logo.png -o logo_autocrop.png\n"
   );
-//! \todo [see _info] add usage examples: 1. add borders, 2. convert .PNG to .TIF
-//! \todo [low] add percentage crop -x% -sx%
   const char* file_i = cimg_option("-i","image","Input image");
-  const char* file_e = cimg_option("-e",".PNG","image extention");
   const char* file_o = cimg_option("-o","_","Output image UBLR");
-  const int x = cimg_option("-x",666,"x crop");
-  const int y = cimg_option("-y",730,"y crop");
-  const int sx = cimg_option("-sx",0,"x size (default 4 quadrants otherwize crop)");
-  const int sy = cimg_option("-sy",0,"y size");
   //help
   const bool help=cimg_option("-h",false,"display this help.");
   //exit on help request '-h' command line option
   if(help) return 0;
-  if(sx==0||sy==0)
-  {//4 quadrant
-  //load source image
-  std::string tmp;
-  tmp=std::string(file_i)+file_e;
-  CImg<unsigned char> image(tmp.c_str());
-  CImg<unsigned char> img;
-  //crop upper left and save it
-  tmp=std::string(file_i)+file_o+std::string("UL")+file_e;
-  img=image.get_crop(0,0,x,y);//image.width()-1,image.height()-1);
-  img.save(tmp.c_str());
-  //crop upper right and save it
-  tmp=std::string(file_i)+file_o+std::string("UR")+file_e;
-  img=image.get_crop(x,0,image.width()-1,y);
-  img.save(tmp.c_str());
-  //crop bottom left and save it
-  tmp=std::string(file_i)+file_o+std::string("BL")+file_e;
-  img=image.get_crop(0,y,x,image.height()-1);
-  img.save(tmp.c_str());
-  //crop bottom right and save it
-  tmp=std::string(file_i)+file_o+std::string("BR")+file_e;
-  img=image.get_crop(x,y,image.width()-1,image.height()-1);
-  img.save(tmp.c_str());
-  }//4 quadrant
-  else
-  {//crop
+  {//autocrop
   //load source image
   CImg<int> image(file_i);
   //crop and save
-  image.crop(x,y,x+sx,y+sy).save(file_o);
+  image.autocrop().save(file_o);
   }//crop
   return 0;
 }
