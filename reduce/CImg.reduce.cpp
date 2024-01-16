1e2fcfc665e0a5a3a426429e90cd4e3d506487e4
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2024-01-16 17:14:11 +0100

    graph: continuous display

--- reduce/CImg.reduce.cpp
+++ reduce/CImg.reduce.cpp
@@ -27,7 +27,7 @@ int main(int argc,char **argv)
         CImg<unsigned char> profimg(image.width(),image.height(),1,1,0);//fill 0
   const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
   //image (lock)
-  image.display(file_i);
+//  image.display(file_i);
   image.print("image");
   //line
   profile=image.get_shared_row(image.height()/2);
@@ -36,15 +36,17 @@ int main(int argc,char **argv)
   disp.assign(image.width(),image.height());
   disp.set_title("live profile");
   image.display(disp);
+  disp.wait(555);
 
   //line (lock)
-  profile.display_graph("middle profile");
+//  profile.display_graph("middle profile");
 
   //line loop
   int i=image.height()/2;
   while( !disp.is_closed() )
   {
     disp.wait(222);
+    disp.set_title("live profile#%d",i);
     profile=image.get_shared_row(i++);
     //profile.print("image(*,i)",false);
     profimg.fill(0).draw_graph(profile,red,1,1,0,255,0).display(disp);
